e6763a1c303a976041e5be272d4ffb873012aef6
worknshubh <worknshubh@gmail.com>
2026-04-08 11:50:14 +0530

    refactor: Remove unused ProjectOverviewCard component from HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -588,30 +588,6 @@ function HomePage() {
                 </svg>
               }
             />
-            <ProjectOverviewCard
-              title="Dataset Size"
-              num="1200"
-              desc="Patients Analyzed"
-              icon={
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                  height="30"
-                  viewBox="0 0 24 24"
-                >
-                  <g
-                    fill="none"
-                    stroke="#6DA179"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                  >
-                    <path d="M14.5 19h-2c-2.829 0-4.243 0-5.121-.879c-.88-.878-.88-2.293-.88-5.121V8c0-2.828 0-4.243.88-5.121C8.256 2 9.67 2 12.499 2h1.344c.818 0 1.226 0 1.594.152c.367.152.656.442 1.234 1.02l2.657 2.656c.578.578.867.868 1.02 1.235c.152.368.152.776.152 1.594V13c0 2.828 0 4.243-.879 5.121C18.743 19 17.328 19 14.5 19" />
-                    <path d="M15 2.5v1c0 1.886 0 2.828.586 3.414c.585.586 1.528.586 3.414.586h1M6.5 5a3 3 0 0 0-3 3v8c0 2.828 0 4.243.878 5.121C5.257 22 6.671 22 9.5 22h5a3 3 0 0 0 3-3M10 11h4m-4 4h7" />
-                  </g>
-                </svg>
-              }
-            />
           </div>
 
           <div className="w-full xl:w-[85%] px-0 sm:px-4 md:px-8 py-4 sm:py-8 md:py-12">
